2b78b10215c71a224255c391529bc4d48732517b
enrico-amerio <amerio.enrico95@gmail.com>
2024-05-31 17:13:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+import axios from 'axios';
+import Card from './components/partials/Card.vue';
+  export default {
+    data(){
+      return{
+        projects: [],
+      }
+    },
+    methods:{
+      getApi(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+          .then(result=>{
+            this.projects = result.data;
+            console.log(this.projects);
+          }).catch(error=>{
+            console.log('errore');
+          });
+      }
+    },
+    mounted(){
+      this.getApi()
+    },
+    components: {
+      Card
+    }
+    
+  }
+</script>
+
+<template>
+  <h1 class="text-center">I miei progetti</h1>
+  <div class="container d-flex flex-wrap align-content-center justify-content-center">
+    <Card v-for="project in projects" :key="project.id"
+        :title="project.title"
+        :type="project.type.name "
+        :description="project.description"
+      />
+    <!-- <div  class="card m-4" style="width: 18rem;" v-for="project in projects" :key="project.id">
+      <ul  class="list-group list-group-flush " >
+        <li class="list-group-item"><strong>Titolo:</strong> {{ project.title }}</li>
+        <li class="list-group-item"><strong>Type:</strong> {{ project.type.name }}</li>
+        <li class="list-group-item"><strong>Description:</strong> {{ project.description }}</li>
+      </ul>
+    </div> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/partials/Card.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    data(){
+      return{
+      }
+    },
+    props:{
+      title: String,
+      type: String,
+      description : String,
+    }
+  };
+</script>
+
+<template>
+  <div class="card m-3 text-center " style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p><strong>Type: </strong>{{ type }}</p>
+      <p><strong>Description: </strong>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card{
+  border: 0px;
+  background-color: rgba(0,124,185, .05);
+  padding: 30px;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  .card-body{
+    margin-top:20px;
+    border-radius: 10px;
+    h5{
+      padding-bottom: 10px;
+      border-bottom: 1px solid black;
+    }
+  }
+ 
+}
+</style>
