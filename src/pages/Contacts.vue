37c347612311ca67b1c55dd5dbe05795ff81daaf
enrico-amerio <amerio.enrico95@gmail.com>
2024-06-05 23:11:14 +0200

    [add] contatto email funzionante

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,30 +1,51 @@
 
 <script>
-import { store } from '../data/store'
+import axios from 'axios';
+import { store } from '../data/store';
   export default {
+    name:'Contacts',
     data(){
       return{
-        store
+        
+        name:'ciao',
+        email:'ciao@ciao.it',
+        message:'ciao ciao ciao'
       }
     },
+    methods:{
+      sendEmail(){
+        const data = {
+          name: this.name,
+          email: this.email,
+          message: this.message
+        }
+        axios.post(store.apiUrl + 'send-email', data)
+        .then(result=>{
+          console.log(result.data);
+        })
+        .catch(error=>{
+          console.log(error.message)
+        })
+      }
+    }
   }
 </script>
  
 <template>
   <div class="container">
     <h1 class="text-center">Contacts</h1>
-    <form>
+    <form @submit.prevent="sendEmail">
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Name</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" >
+        <input v-model="name" name="name" type="text" class="form-control" id="exampleFormControlInput1" >
       </div>
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1">
+        <input v-model="email" name="email" type="email" class="form-control" id="exampleFormControlInput1">
       </div>
       <div class="mb-3">
         <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea v-model="message" name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
       <button type="submit" class="btn btn-primary">Send</button>
     </form>
